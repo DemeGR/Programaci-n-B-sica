a54b9c4b555393390c9c39907382d06cc1a0d249
DemeGR <gurd970714@gs.utm.mx>
2022-08-03 10:52:11 -0500

    Video 20: addEventListener

--- mokepon/js/mokepon.js
+++ mokepon/js/mokepon.js
@@ -1,6 +1,24 @@
+function iniciarJuego(){
+    let botonMascotaJugador = document.getElementById('boton-mascota')
+    botonMascotaJugador.addEventListener('click', seleccionarMascotaJugador)
+}
 function seleccionarMascotaJugador(){
-    alert("SELECCIONASTE TU MASCOTA")
+   let inputHipodoge = document.getElementById('hipodoge')
+   let inputCapipepo = document.getElementById('capipepo')
+   let inputRatigueya = document.getElementById('ratigueya')
+
+   if(inputHipodoge.checked){
+        alert('Seleccionaste a Hipodoge')
+   } else if(inputCapipepo.checked){
+        alert('Seleccionaste a Capipepo')
+   } else if(inputRatigueya.checked){
+        alert('Seleccionaste a Ratigueya')
+   }else{
+        alert('Selecciona a una mascota')
+   }
 }
 
-let botonMascotaJugador = document.getElementById('boton-mascota')
-botonMascotaJugador.addEventListener('click', seleccionarMascotaJugador)
+
+//confirmar que el html haya cargado todo, despues, llamar 
+//iniciarJuego()
+window.addEventListener('load', iniciarJuego)
