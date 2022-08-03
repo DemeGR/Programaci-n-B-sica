9d973618cb0037605298e8d652bdb83670943b9b
DemeGR <gurd970714@gs.utm.mx>
2022-08-03 14:18:18 -0500

    Video 22: Enemigos aleatorios

--- mokepon/js/mokepon.js
+++ mokepon/js/mokepon.js
@@ -8,7 +8,9 @@ function seleccionarMascotaJugador(){
    let inputRatigueya = document.getElementById('ratigueya')
    let spanMascotaJugador = document.getElementById('mascota-jugador')
 
-   if(inputHipodoge.checked){
+   //checar que mascota se selecciono y mostrarlo en 
+   //la seccion seleccionar-ataque
+   if(inputHipodoge.checked){//checked checa si es falso o true el ratio
         spanMascotaJugador.innerHTML ='Hipodoge'
    } else if(inputCapipepo.checked){
         spanMascotaJugador.innerHTML ='Capipepo'
@@ -17,6 +19,28 @@ function seleccionarMascotaJugador(){
    }else{
         alert('Selecciona a una mascota')
    }
+
+   //seleccion del enemigo
+   seleccionarMascotaEnemigo()
+}
+
+//funcion seleccionar mascota enemigo
+function seleccionarMascotaEnemigo(){
+    let ataqueAleatorio = aleatorio(1,3)
+    let spanMascotaEnemigo = document.getElementById('mascota-enemigo')
+
+    if(ataqueAleatorio == 1){
+        spanMascotaEnemigo.innerHTML = 'Hipodoge' //se inserta en el html el nombre de la mascota 
+    }else if(ataqueAleatorio == 2){
+        spanMascotaEnemigo.innerHTML = 'Capipepo'
+    }else{
+        spanMascotaEnemigo.innerHTML = 'Ratigueya'
+    }
+}
+
+//numero aleatorio
+function aleatorio(min, max){
+    return Math.floor(Math.random() * (max - min + 1) + min)
 }
 
 
