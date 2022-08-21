8971cbc67c9c5573cfb4f84f82b0cfddc12e2768
DemeGR <gurd970714@gs.utm.mx>
2022-08-21 00:25:39 -0500

    Video 74. Servidor wed con Express.js .

--- mokepon/index.js
+++ mokepon/index.js
@@ -1,1 +1,11 @@
-console.log("Hola Node")
+const express  = require("express")//1.-se importa express para poder utilizarlo en el proyecto
+
+const app = express()//2.-se crea una aplicion con express
+//url y cómo se respondera a la solicitud
+app.get("/",(req/*peticion*/ , res/*respuesta*/ )=>{//3.-cuando se resive una petición en la url raiz que se responda "Hola"
+    res.send("Hola")
+})
+
+app.listen(8080, ()=>{//4.-que escuche continuamente en el puerto 8080 las peticiones de los cleintes para que le pueda responder
+    console.log("Servidor funcionando")
+})
